40a321d638cc830eccb15d35fc77fb2dfcd77b11
mikeyshriner <shrinermichael@gmail.com>
2019-10-27 19:53:57 -0400

    beatify4

--- src/grades.js
+++ src/grades.js
@@ -8,18 +8,21 @@ let QuizC = readlineSync.question("");
 let Test = readlineSync.question("\nEnter three test grades.\n")
 let TestB = readlineSync.question("");
 let TestC = readlineSync.question("");
-const HomeworkWGHT = Homework*0.15
-const HomeworkBWGHT = HomeworkB*0.15
-const HomeworkCWGHT = HomeworkC*0.15
-const QuizWGHT = Quiz*0.35
-const QuizBWGHT = QuizB*0.35
-const QuizCWGHT = QuizC*0.35
-const TestWGHT = Test*0.5
-const TestBWGHT = TestB*0.5
-const TestCWGHT = TestC*0.5
-const HomeworkAVG = (HomeworkWGHT + HomeworkBWGHT + HomeworkCWGHT)/3
-const QuizAVG = (QuizWGHT + QuizBWGHT + QuizCWGHT)/3
-const TestAVG = (TestWGHT + TestBWGHT + TestCWGHT)/3
+const HomeworkWGHT = Homework * 0.15
+const HomeworkBWGHT = HomeworkB * 0.15
+const HomeworkCWGHT = HomeworkC * 0.15
+const QuizWGHT = Quiz * 0.35
+const QuizBWGHT = QuizB * 0.35
+const QuizCWGHT = QuizC * 0.35
+const TestWGHT = Test * 0.5
+const TestBWGHT = TestB * 0.5
+const TestCWGHT = TestC * 0.5
+const HomeworkAVG = (HomeworkWGHT + HomeworkBWGHT + HomeworkCWGHT) / 3
+const QuizAVG = (QuizWGHT + QuizBWGHT + QuizCWGHT) / 3
+const TestAVG = (TestWGHT + TestBWGHT + TestCWGHT) / 3
 let Grade = (HomeworkAVG + QuizAVG + TestAVG)
-Grade = Grade.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});;
-console.log("\nYour marking period grade is " + Grade + "%." );
+Grade = Grade.toLocaleString("en", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+console.log("\nYour marking period grade is " + Grade + "%.");
