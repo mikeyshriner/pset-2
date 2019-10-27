245ae2341990f565c335868f229d854c6e1b8e4e
mikeyshriner <shrinermichael@gmail.com>
2019-10-27 19:52:55 -0400

    beautify3

--- src/diagonal.js
+++ src/diagonal.js
@@ -1,7 +1,10 @@
 const readlineSync = require("readline-sync");
 let Width = readlineSync.question("\nWidth: ");
 let Length = readlineSync.question("Length: ");
-let Diagonal1 = (Length*Length)+(Width*Width)
+let Diagonal1 = (Length * Length) + (Width * Width)
 let Diagonal = Math.sqrt(Diagonal1)
-Diagonal = Diagonal.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});
+Diagonal = Diagonal.toLocaleString("en", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
 console.log("\nA(n) " + Width + "-by-" + Length + " inch sheet of paper has a diagonal of " + Diagonal + " inch(es). ");
