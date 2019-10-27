c206d2c192de49b141dbbb48b0b130d966540b5c
mikeyshriner <shrinermichael@gmail.com>
2019-10-27 19:57:48 -0400

    sddshuvdsbj

--- src/salary.js
+++ src/salary.js
@@ -1,8 +1,11 @@
 const readlineSync = require("readline-sync");
 let Salary = readlineSync.question("\nAnnual salary: ");
-const SalaryBM = Salary/24
-const PreTax = SalaryBM - SalaryBM*0.07
-const Tax = PreTax*0.2782
+const SalaryBM = Salary / 24
+const PreTax = SalaryBM - SalaryBM * 0.07
+const Tax = PreTax * 0.2782
 let Pay = PreTax - Tax
-Pay = Pay.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});
+Pay = Pay.toLocaleString("en", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
 console.log("\nYour take-home pay each check will be $" + Pay + ".");
